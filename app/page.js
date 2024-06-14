3ecc239d3f9d64110d90fab2c0e3fad20a5e64c5
Francis Bell <francisbell@franciss-mbp.mynetworksettings.com>
2024-06-14 13:57:18 -0400

    remove quote

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
               <Image src="/img/bitmoji.png" className="card-img-top" width={500} height={500} alt="Test Image" />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
                 <a href="#" className="btn btn-primary">
                   Go somewhere
                 </a>
@@ -25,7 +25,7 @@ export default function Home() {
               <Image src="/img/bitmoji.png" className="card-img-top" width={500} height={500} alt="Test Image" />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
                 <a href="#" className="btn btn-primary">
                   Go somewhere
                 </a>
@@ -37,7 +37,7 @@ export default function Home() {
               <Image src="/img/bitmoji.png" className="card-img-top" width={500} height={500} alt="Test Image" />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
                 <a href="#" className="btn btn-primary">
                   Go somewhere
                 </a>
